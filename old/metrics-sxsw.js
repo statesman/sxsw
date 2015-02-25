111f7caff1f4903593bba8ae0f034634cb436484
Christian McDonald <cmcdonald@statesman.com>
2015-02-25 14:01:35 -0600

    removing old files

--- old/metrics-sxsw.js
+++
@@ -1,7 +0,0 @@
-<!-- project specfic stuff -->
-<script language="JavaScript" type="text/javascript"><!--
-var projectSite = "austin360";
-var projectChannel = "events";
-var projectCategory = "festivals";
-var projectSubCategory = "";
---> </script>
